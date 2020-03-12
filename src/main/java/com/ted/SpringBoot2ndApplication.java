9e6104e56dd6dad38e78ed44b198ac01d56deb88
ted <ted@tist.com.tw>
2020-03-12 16:43:06 +0800

    spring-boot-2nd

---
+++ src/main/java/com/ted/SpringBoot2ndApplication.java
@@ -0,0 +1,13 @@
+package com.ted;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBoot2ndApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBoot2ndApplication.class, args);
+    }
+
+}
